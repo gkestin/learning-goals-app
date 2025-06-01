cc378425f97f9d080e47a5e951cfb59a58d08cc7
gkestin <greg.kestin@gmail.com>
2025-06-01 10:36:33 -0400

    change max size upload v3

--- deploy.sh
+++ deploy.sh
@@ -47,6 +47,7 @@ gcloud run deploy $SERVICE_NAME \
   --cpu=2 \
   --timeout=3600 \
   --execution-environment=gen2 \
+  --use-http2 \
   --set-env-vars "SECRET_KEY=learning-goals-secret-key-production" \
   --set-env-vars "OPENAI_API_KEY=$OPENAI_API_KEY" \
   --set-env-vars "FIREBASE_STORAGE_BUCKET=learninggoals2.firebasestorage.app" \
